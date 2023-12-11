ad41bdbd054f16d7d9739440b102dab62e6ecd9a
Rick <Rick.van.rooijen@movares.nl>
2023-12-11 19:11:02 +0100

    Day 3

--- AdventOfCode.Tests/Day3.cs
+++ AdventOfCode.Tests/Day3.cs
@@ -38,17 +38,41 @@ public class Day3
             return sum;
         }
 
-        /*public int Calculate2()
+        public int Calculate2()
         {
-            // If this gear has 2 adjacent 
+            int sum = 0;
             foreach ((int x, int y) gear in _gears)
-            {
+            { 
+                var adjacent = GetAdjacent(gear.x, gear.y);
+                
+                // Check all numbers for each gear
+                List<int> matchedNumber = new List<int>();
                 foreach ((int startX, int startY, int count, int number) number in _numbers)
                 {
-                    
+                    // Does any part of the number overlap with the adjacent of the gear?
+                    int startX = number.startX;
+                    int endX = number.startX + number.count;
+                    int y = number.startY;
+
+                    // Move over x to check if it matches adjacent
+                    for (int x = startX; x < endX; x++)
+                    {
+                        if (adjacent.Exists(tuple => tuple.x == x && tuple.y == y))
+                        {
+                            matchedNumber.Add(number.number);
+                            break;
+                        }
+                    }
+                }
+
+                if (matchedNumber.Count == 2)
+                {
+                    sum += matchedNumber[0] * matchedNumber[1];
                 }
             }
-        }*/
+
+            return sum;
+        }
 
         private void PopuplateNumbers()
         {
@@ -85,9 +109,25 @@ public class Day3
                     }
                 }
             }
-            
         }
 
+        List<(int x, int y)> GetAdjacent(int x, int y)
+        {
+            return new List<(int x, int y)>()
+            {
+                new(x - 1, y + 1),
+                new(x, y + 1),
+                new(x + 1, y + 1),
+
+                new(x - 1, y),
+                new(x + 1, y),
+
+                new(x - 1, y - 1),
+                new(x, y - 1),
+                new(x + 1, y - 1),
+            };
+        }
+        
         List<char> GetAdjacent(int x, int y, int size)
         {
             var adjacent = new List<char>();
@@ -120,6 +160,13 @@ public class Day3
     [Test]
     public void Part2()
     {
+        int answer = 539590;
+        var schematic = new Schematic();
+
+        int x = schematic.Calculate2();
         
+        Console.WriteLine(x);
+        
+        Assert.Pass();
     }
 }
